f853bb404284e41b5583f950a7cb52bfcfdd0920
ljchueca <72858631+ljchueca@users.noreply.github.com>
2024-02-23 13:53:33 +0100

    Add files via upload

---
+++ SLURM_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=launch_to_slurm
+#SBATCH --error %x-%j.err
+#SBATCH --output %x-%j.out
+
+#SBATCH --reservation=mer2
+#SBATCH --partition=vfast
+#SBATCH --mem=2G
+#SBATCH --ntask=2
+
+module load fastqc
+
+awk '{print$2,$1}' species.list4 > newlist.txt
+
+cp newlist.txt output.result
+
+rm newlist.txt
+
